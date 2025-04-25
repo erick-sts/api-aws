c73c5ad84eac6e903d39c90fdd961c0fc67c1ff5
unknown <erick.sts.eo@gmail.com>
2025-04-24 22:12:25 -0300

    fix name

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch(err => logError('Erro ao logar mongodb' + err, null, err));
 
 const UserSchema = new mongoose.Schema({
-    nome: String,
+    name: String,
     email: String
 });
 
